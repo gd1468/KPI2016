267e31f9fd55e08d67e7cb62612a3e93aee4ffb0
Dai Nguyen <gd1468@gmail.com>
2016-09-08 10:40:35 +0700

    add login form

--- MoneyManagement/MoneyManagement.Web/Scripts/angular/directives/angular-directives.js
+++ MoneyManagement/MoneyManagement.Web/Scripts/angular/directives/angular-directives.js
@@ -1,18 +1,33 @@
 ﻿(function () {
     'use strict';
     angular.module("mmHomeApp", ["ngRoute"])
-        .directive("mmPageHeader",
-             function () {
-                 return {
-                     restrict: 'E',
+        .directive("mmPageHeader", function () {
+            return {
+                restrict: 'E',
 
-                     transclude: true,
-                     replace: true,
-                     scope: {
+                transclude: true,
+                replace: true,
+                scope: {
 
-                     },
-                     templateUrl: '/Scripts/angular/templates/mmPageHeader.html'
-                 }
-             }
-        );
+                },
+                templateUrl: '/Scripts/angular/templates/mmPageHeader.html'
+            }
+        })
+        .directive('mmLoginForm', function () {
+            return {
+                restrict: 'E',
+                transclude: true,
+                replace: true,
+                scope: {
+                    userName: "=",
+                    passWord: "="
+                },
+                templateUrl: '/Scripts/angular/templates/mmLoginForm.html',
+                controller: ['$scope', function ($scope) {
+                    $scope.submit = function() {
+                        window.alert("Welcome " + $scope.userName);
+                    };
+                }]
+            }
+        });
 })();
